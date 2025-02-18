1f57e287e527cd732e548a9f2613bcf7622f6ad6
dojinss <ehwls20@gmail.com>
2025-02-18 17:16:29 +0900

    Updates

--- freeorder/SQL/DDL/USER_AUTH.sql
+++
@@ -1,7 +0,0 @@
-drop table if exists `user_auth`;
-
-create table `user_auth` (
-    `no` int primary key AUTO_INCREMENT,
-    `username` varchar(100) not null,
-    `auth` varchar(100) not null
-);
