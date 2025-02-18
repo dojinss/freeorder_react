76208744b5bbc20f34fc893317aa4d0860e38210
dojinss <ehwls20@gmail.com>
2025-02-18 17:10:47 +0900

    Deploy to GitHub Pages

--- pos-app/vite.config.js
+++ pos-app/vite.config.js
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "./",
   plugins: [react()],
   define: {
     global: {}, // 브라우저 환경에서 빈 global 객체 정의
@@ -14,13 +15,13 @@ export default defineConfig({
     // 프록시 설정
     proxy: {
       "/api": {
-        target: "http://localhost:8080", // (port) 서버 주소
+        target: "http://dojinss.cafe24.com/", // (port) 서버 주소
         changeOrigin: true, // 요청헤더의 Host도 변경
         secure: false, // https 지원 여부
         rewrite: (path) => path.replace(/^\/api/, ""),
       },
       "/ws": {
-        target: "http://localhost:8080", // (port) 서버 주소
+        target: "http://dojinss.cafe24.com/", // (port) 서버 주소
         changeOrigin: true, // 요청헤더의 Host도 변경
         ws: true, // WebSocket을 위한 프록시 설정
         secure: false, // https 지원 여부
